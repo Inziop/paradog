aa3ae29f3fbcc99e7189c457673eb5d147dcba8c
Nguyen Minh Hoang <minhhoangprovjp123@gmail.com>
2025-10-25 11:33:53 +0700

    update

---
+++ ExportOptionsWindow.xaml.cs
@@ -0,0 +1,33 @@
+using System.Windows;
+
+namespace ParadoxTranslator;
+
+public partial class ExportOptionsWindow : Window
+{
+    public enum ExportFormat { Csv, Json, Yaml }
+
+    public ExportFormat SelectedFormat { get; private set; } = ExportFormat.Csv;
+    public bool ExportAllFiles { get; private set; } = true;
+
+    public ExportOptionsWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnContinue(object sender, RoutedEventArgs e)
+    {
+        if (CsvRadio.IsChecked == true) SelectedFormat = ExportFormat.Csv;
+        else if (JsonRadio.IsChecked == true) SelectedFormat = ExportFormat.Json;
+        else SelectedFormat = ExportFormat.Yaml;
+
+        ExportAllFiles = AllFilesRadio.IsChecked == true;
+        DialogResult = true;
+        Close();
+    }
+
+    private void OnCancel(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+}
